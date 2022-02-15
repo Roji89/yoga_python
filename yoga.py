c2cb5e7a52a31826ca5d87a5c151b62e05bd3c31
Roji89 <roja.gh89@gmail.com>
2022-02-15 08:58:37 +0100

    register

--- yoga.py
+++ yoga.py
@@ -41,7 +41,7 @@ def close_db(e=None):
 
 app.teardown_appcontext(close_db)
 app.cli.add_command(init_db_command)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'    
+app.secret_key ='_5#y2L"192b9bdd22ab9\n\xec]/'    
 
 @app.route('/', methods=('GET', 'POST')) 
 def welcome():
